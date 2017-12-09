406186e75b320e90202245507944c3f3ae9ffc31
AdamLe <duongle92vn@gmail.com>
2017-12-09 14:12:08 +0200

    GoalVC Layout

---
+++ goal-post/Controller/GoalVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalVC.swift
+//  goal-post
+//
+//  Created by Le Dang Dai Duong on 12/9/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+class GoalVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
+    }
+    
+    
+}
