01e7cc576c2e86d0f887d3edb637fa9b1b31e959
AdamLe <duongle92vn@gmail.com>
2017-12-09 18:42:24 +0200

    UIButton extension for CreateGoalVC

---
+++ goal-post/Extensions/UIButtonExt.swift
@@ -0,0 +1,20 @@
+//
+//  UIButtonExt.swift
+//  goal-post
+//
+//  Created by Le Dang Dai Duong on 12/9/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    
+    func setSelectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.4037566554, green: 0.7161955114, blue: 0.4686756243, alpha: 1)
+    }
+    
+    func setDeselectedColor() {
+        self.backgroundColor = #colorLiteral(red: 0.6003380566, green: 0.865584788, blue: 0.6334497222, alpha: 1)
+    }
+}
---
+++ goal-post/Extensions/UIViewExt.swift
@@ -0,0 +1,42 @@
+//
+//  UIViewExt.swift
+//  goal-post
+//
+//  Created by Le Dang Dai Duong on 12/9/17.
+//  Copyright © 2017 Le Dang Dai Duong. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func bindToKeyboard() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(_:)), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+    }
+    
+    @objc func keyboardWillChange(_ notification: NSNotification) {
+        let duration = notification.userInfo![UIKeyboardAnimationDurationUserInfoKey] as! Double
+        // Ease in, ease out
+        let curve = notification.userInfo![UIKeyboardAnimationCurveUserInfoKey] as! UInt
+        let startingFrame = (notification.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let endingFrame = (notification.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        let deltaY = endingFrame.origin.y - startingFrame.origin.y
+        
+        UIView.animate(withDuration: duration, delay: 0.0, options: UIViewAnimationOptions(rawValue: curve), animations: {
+            self.frame.origin.y += deltaY
+        }, completion: nil)
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
